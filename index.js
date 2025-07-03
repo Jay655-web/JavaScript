2830b98c74643d8ef8b9b1ba78a659c88fa4268e
unknown <adjeiedmond582@gmail.com>
2025-07-03 14:58:35 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+// COUNTER PROGRAM
+// const decrease = document.getElementById(decreasebtn);
+// const reset = document.getElementById(resetbtn);
+// const increase = document.getElementById(increasebtn);
+// const label = document.getElementById(countlabel);
+// let count = 0;
+// increasebtn.onclick = function(){
+//     count++;
+//     countlabel.textContent = count;
+
+//     console.log(count)
+// }
+// decreasebtn.onclick = function(){
+//     count--;
+//     countlabel.textContent = count;
+
+//     console.log(count)
+// }
+// resetbtn.onclick = function(){
+//     count=0;
+//     countlabel.textContent = count;
+
+//     console.log(count)
+// }
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
