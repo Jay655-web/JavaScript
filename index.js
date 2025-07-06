bc602b17203fe17c94bff24ce2a164b4142bdda2
Adjei Edmond <adjeiedmond582@gmai.com>
2025-07-06 00:11:05 +0000

    changed the whole code to try checkboxes and radio buttons

--- index.js
+++ index.js
@@ -1,35 +1,54 @@
-// COUNTER PROGRAM
-// const decrease = document.getElementById(decreasebtn);
-// const reset = document.getElementById(resetbtn);
-// const increase = document.getElementById(increasebtn);
-// const label = document.getElementById(countlabel);
-// let count = 0;
-// increasebtn.onclick = function(){
-//     count++;
-//     countlabel.textContent = count;
+// const   myCheckbox = document.getElementById("mycheckbox");
+// const   mySubmit = document.getElementById("mysubmit");
+// const   subResult = document.getElementById("subresult");
+// const   paymentResult = document.getElementById("paymentresult");
 
-//     console.log(count)
+// mySubmit.onclick = function(){
+//     if(myCheckbox.checked){
+//         subResult.textContent = "Thank you for subscribing!";
+//     }else{
+//         subResult.textContent = "You are not subscribed.";
+//     }
+
+//     if(document.getElementById("visabtn").checked){
+//         paymentResult.textContent = "You have selected Visa.";
+//     }else if(document.getElementById("mastercardbtn").checked){
+//         paymentResult.textContent = "You have selected MasterCard.";
+//     }else if(document.getElementById("paypalbtn").checked){
+//         paymentResult.textContent = "You have selected PayPal.";
+//     }else{
+//         paymentResult.textContent = "No payment method selected.";
+//     }
 // }
-// decreasebtn.onclick = function(){
-//     count--;
-//     countlabel.textContent = count;
 
-//     console.log(count)
-// }
-// resetbtn.onclick = function(){
-//     count=0;
-//     countlabel.textContent = count;
+const myCheckbox = document.getElementById("mycheckbox");
+const visaBtn = document.getElementById("visabtn");
+const masterCardbtn = document.getElementById("mastercardbtn");
+const payPalbtn = document.getElementById("paypalbtn");
+const mySubmit = document.getElementById("mysubmit");
+const subResult = document.getElementById("subresult");
+const paymentResult = document.getElementById("paymentresult");
 
-//     console.log(count)
-// 
-// GREETING PROGRAM
-let name1 = document.getElementById("name").value;
-let btn = document.getElementById("btn");
-let newh = document.getElementById("htext");
 
-btn.onclick = function(){
-    newh.textContent = `Hello ${name1}, Welcome to the site.`;
+mySubmit.onclick = function(){
+   
+    if(myCheckbox.checked){
+        subResult.textContent = "You are subscribed!";
+    }
+    else{
+        subResult.textContent = "You are NOT subscribed!";
+    }
+
+    if(visaBtn.checked){
+        paymentResult.textContent = "You are paying with Visa.";
+    }
+    else if(masterCardbtn.checked){
+        paymentResult.textContent = "You are paying with MasterCard.";
+    }
+    else if(payPalbtn.checked){
+        paymentResult.textContent = "You are paying with PayPal.";
+    }
+    else{
+        paymentResult.textContent = "You must select the payment type"
+    }
 }
-
-
-
